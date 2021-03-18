b09f5c2a5c666298d6af9ebe20246ea6e7b757b3
afaq-AA <18134119-027@uog.edu.pk>
2021-03-18 10:36:01 +0500

    backend

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://exampledata:mongodb@cluster0.j15k9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000), console.log('server is running'))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies')); 
+
+app.use(authRoutes);
+
+
