37834b2fa6d1447c2b8600b8a37dccd2662690c5
afaq-AA <18134119-027@uog.edu.pk>
2021-03-18 11:35:40 +0500

    added schema user

--- controllers/authControlers.js
+++ controllers/authControlers.js
@@ -1,4 +1,6 @@
-
+const User = require("../models/User");
+const user = User()
+// const user = require(User)
 
 
 
@@ -11,13 +13,22 @@ module.exports.login_get = (req,res) =>{
     res.render('login');
 }
 
-module.exports.signup_post = (req,res) =>{
-    console.log(req.body);  
-    res.send('New signup');
+module.exports.signup_post = async (req,res) =>{
+
+    const {email, password} = req.body;
+    try{
+        const user = await User.create({email,password});
+        res.status(201).json(user)
+
+    }
+    catch (err ){
+        console.log(err);
+        res.status(400).send('error, user not created')
+    }
 }
 
-module.exports.login_post = (req,res) =>{
-    console.log(req.body);
+module.exports.login_post = async (req,res) =>{
+    const {email, password} = req.body;
+    console.log(email, password);
     res.send('User login');
 }
-
