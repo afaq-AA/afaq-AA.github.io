2d607ad81d38394138c0f67bb14380994078671d
afaq-AA <18134119-027@uog.edu.pk>
2021-03-21 22:37:10 +0500

    json web token created in cookie

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser')
 
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
@@ -22,5 +24,21 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies')); 
 
 app.use(authRoutes);
+// //cookies
+// app.get( '/set-cookie', (req,res)=>{
+//   //res.setHeader('set-cookie','newUser= true')
+//   res.cookie('newUser', false)
+//   res.cookie('isEmployee',true, {maxAge: 1000*60*60*24 , httpOnly: true })
+//   res.send('you got the cookies');
+
+// });
+// app.get('/read-cookie', (req,res)=> {
+//   const cookies = req.cookies
+//   console.log(cookies.newUser );
+//   res.json(cookies);
+
+
+// }); 
+
 
 
